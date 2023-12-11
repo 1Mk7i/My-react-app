bfe1f53840a9465bb1c607312de356b46e0b3127
1Mk7i <127222921+1Mk7i@users.noreply.github.com>
2023-12-11 12:54:50 +0200

    mess

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import './styles/Mediacode.css';
 import './styles/Header.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
